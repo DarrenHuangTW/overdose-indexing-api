3a9b7985c6a85ef09c54fe1cb5d59dac7f50cda5
DarrenHuangTW <darrenhhuang@gmail.com>
2023-11-08 20:57:35 +0800

    add option to upload their own secrets

--- app.py
+++ app.py
@@ -10,62 +10,90 @@ import streamlit as st
 st.header("Google Indexing API - Overdose", divider='rainbow')
 
 
-
-
-# Let users upload their secrets, which is a json file
-# uploaded_file = st.file_uploader("Upload your secrets (JSON file)", type="json")
-# if uploaded_file is not None:
-#     secrets = json.load(uploaded_file)
-#     service_account_info = secrets
-#     credentials = service_account.Credentials.from_service_account_info(
-#         service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
-#     )
-#     google_client = build("indexing", "v3", credentials=credentials)
-
-
-# STEP 1
-st.markdown("### Step 1: Select a service account from the list below.")
-all_secrets = st.secrets
-selected_secret = st.selectbox('ach account has a daily quota of 200 URLs. If one fails, please select another.', list(all_secrets.keys()))
-secrets = all_secrets[selected_secret]
-
-service_account_info = {
-    "type": secrets["type"],
-    "project_id": secrets["project_id"],
-    "private_key_id": secrets["private_key_id"],
-    "private_key": secrets["private_key"],
-    "client_email": secrets["client_email"],
-    "client_id": secrets["client_id"],
-    "auth_uri": secrets["auth_uri"],
-    "token_uri": secrets["token_uri"],
-    "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": secrets["client_x509_cert_url"],
-}
-
-# STEP 2
-st.markdown("### Step 2: Add the following account as a delegated owner in Google Search Console for the website for which you wish to submit URLs:")
-client_email = secrets["client_email"]
-st.markdown(client_email)
-
-
-credentials = service_account.Credentials.from_service_account_info(
-    service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
-)
-
-google_client = build("indexing", "v3", credentials=credentials)
+option = st.radio("Select an option:", ("Use a shared service account", "Upload JSON secrets"))
 
 
 
+if option == "Upload JSON secrets":
+    # Let users upload their secrets, which is a JSON file
+    # STEP 2
+    st.markdown("### Step 2: Upload the JSON secrets file for your service account below:")
+    uploaded_file = st.file_uploader("", type="json")
+    if uploaded_file is not None:
+        secrets = json.load(uploaded_file)
+        service_account_info = secrets
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
+        )
+        google_client = build("indexing", "v3", credentials=credentials)
+        client_email = secrets["client_email"]
 
-# Use file
-# st.markdown("## Steps:")
-# steps = ''':one: Add 'seo-admin@darren-indexing-api.iam.gserviceaccount.com' as a delegated owner in Google Search Console for the website you'd like to submit
 
-# :two: Provide a list of URL you'd like to request indexing
+elif option == "Use a shared service account":
+    # Display the list of service accounts and let users select one
+    
+    # STEP 1
+    st.markdown("### Step 1: Select a service account from the list below.")
+    all_secrets = st.secrets
+    selected_secret = st.selectbox('Each account has a daily quota of 200 URLs. If one fails, please select another.', list(all_secrets.keys()))
+    secrets = all_secrets[selected_secret]
 
-# :three: Submit
-# '''
-# st.markdown(steps)
+    service_account_info = {
+        "type": secrets["type"],
+        "project_id": secrets["project_id"],
+        "private_key_id": secrets["private_key_id"],
+        "private_key": secrets["private_key"],
+        "client_email": secrets["client_email"],
+        "client_id": secrets["client_id"],
+        "auth_uri": secrets["auth_uri"],
+        "token_uri": secrets["token_uri"],
+        "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": secrets["client_x509_cert_url"],
+    }
+
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
+    )
+
+    google_client = build("indexing", "v3", credentials=credentials)
+
+    # STEP 2
+    st.markdown("### Step 2: Add the following account as a delegated owner in Google Search Console for the website for which you wish to submit URLs:")
+    client_email = secrets["client_email"]
+    st.markdown(client_email)
+
+
+# # STEP 1
+# st.markdown("### Step 1: Select a service account from the list below.")
+# all_secrets = st.secrets
+# selected_secret = st.selectbox('Each account has a daily quota of 200 URLs. If one fails, please select another.', list(all_secrets.keys()))
+# secrets = all_secrets[selected_secret]
+
+# service_account_info = {
+#     "type": secrets["type"],
+#     "project_id": secrets["project_id"],
+#     "private_key_id": secrets["private_key_id"],
+#     "private_key": secrets["private_key"],
+#     "client_email": secrets["client_email"],
+#     "client_id": secrets["client_id"],
+#     "auth_uri": secrets["auth_uri"],
+#     "token_uri": secrets["token_uri"],
+#     "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+#     "client_x509_cert_url": secrets["client_x509_cert_url"],
+# }
+
+# # STEP 2
+# st.markdown("### Step 2: Add the following account as a delegated owner in Google Search Console for the website for which you wish to submit URLs:")
+# client_email = secrets["client_email"]
+# st.markdown(client_email)
+
+
+# credentials = service_account.Credentials.from_service_account_info(
+#     service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
+# )
+
+# google_client = build("indexing", "v3", credentials=credentials)
+
 
 # STEP 3 
 st.markdown("### Step 3: Provide a list of URLs that you would like to request indexing for, then submit.")
