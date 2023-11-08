6cc18b7eb28d047cffa5f7bd40b81b49eb4bc340
DarrenHuangTW <darrenhhuang@gmail.com>
2023-11-08 21:05:30 +0800

    clean up codes

--- app.py
+++ app.py
@@ -63,38 +63,6 @@ elif option == "Use a shared service account":
     st.markdown(client_email)
 
 
-# # STEP 1
-# st.markdown("### Step 1: Select a service account from the list below.")
-# all_secrets = st.secrets
-# selected_secret = st.selectbox('Each account has a daily quota of 200 URLs. If one fails, please select another.', list(all_secrets.keys()))
-# secrets = all_secrets[selected_secret]
-
-# service_account_info = {
-#     "type": secrets["type"],
-#     "project_id": secrets["project_id"],
-#     "private_key_id": secrets["private_key_id"],
-#     "private_key": secrets["private_key"],
-#     "client_email": secrets["client_email"],
-#     "client_id": secrets["client_id"],
-#     "auth_uri": secrets["auth_uri"],
-#     "token_uri": secrets["token_uri"],
-#     "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
-#     "client_x509_cert_url": secrets["client_x509_cert_url"],
-# }
-
-# # STEP 2
-# st.markdown("### Step 2: Add the following account as a delegated owner in Google Search Console for the website for which you wish to submit URLs:")
-# client_email = secrets["client_email"]
-# st.markdown(client_email)
-
-
-# credentials = service_account.Credentials.from_service_account_info(
-#     service_account_info, scopes=["https://www.googleapis.com/auth/indexing"]
-# )
-
-# google_client = build("indexing", "v3", credentials=credentials)
-
-
 # STEP 3 
 st.markdown("### Step 3: Provide a list of URLs that you would like to request indexing for, then submit.")
 urls_input = st.text_area("Enter up to 100 URLs you wish to submit, one URL per line.")
